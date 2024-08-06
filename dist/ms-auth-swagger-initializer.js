f7f5805d1925652ef1bfa905b3997dd9898e6800
gabrielga-dev <dealmeida.gabriel.d@gmail.com>
2024-08-05 23:55:08 -0300

    add ms-auth

--- dist/ms-auth-swagger-initializer.js
+++ dist/ms-auth-swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "ms-auth-doc.yaml",
+    url: "ms_auth_doc.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
